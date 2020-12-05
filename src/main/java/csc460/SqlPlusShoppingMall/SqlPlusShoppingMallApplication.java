ea78eec8d3f89faa9650338e19b7c19422e63789
ChaonengQuan <chaonengquan@email.arizona.edu>
2020-12-05 11:30:55 -0700

    Add datasource

--- src/main/java/csc460/SqlPlusShoppingMall/SqlPlusShoppingMallApplication.java
+++ src/main/java/csc460/SqlPlusShoppingMall/SqlPlusShoppingMallApplication.java
@@ -2,9 +2,23 @@ package csc460.SqlPlusShoppingMall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SqlPlusShoppingMallApplication {
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName(oracle.jdbc.driver.OracleDriver.class.getName());
+		ds.setUrl("jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle");
+		ds.setUsername("");
+		ds.setPassword("");
+		return ds;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SqlPlusShoppingMallApplication.class, args);
