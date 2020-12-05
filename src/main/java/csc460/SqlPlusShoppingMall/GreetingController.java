dd9523ff51330915588eb52a2a4d4e68272e8f31
ChaonengQuan <chaonengquan@email.arizona.edu>
2020-12-05 11:27:20 -0700

    Add ojdbc.jar Update pom.xml

---
+++ src/main/java/csc460/SqlPlusShoppingMall/GreetingController.java
@@ -0,0 +1,38 @@
+package csc460.SqlPlusShoppingMall;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+@Controller
+public class GreetingController {
+
+    @Autowired
+    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
+
+    @PostConstruct
+    private void postConstruct() {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
+    }
+
+}
