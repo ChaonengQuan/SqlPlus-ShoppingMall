deeb72490d7f37c4690b9a6d1e6b386cc46fe111
ChaonengQuan <chaonengquan@email.arizona.edu>
2020-12-05 11:18:47 -0700

    Add starter code for Spring application

---
+++ src/main/java/csc460/SqlPlusShoppingMall/SqlPlusShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package csc460.SqlPlusShoppingMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlPlusShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlPlusShoppingMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csc460/SqlPlusShoppingMall/SqlPlusShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package csc460.SqlPlusShoppingMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlPlusShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
