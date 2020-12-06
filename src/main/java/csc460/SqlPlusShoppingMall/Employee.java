b630a1cb970c2426a6975711ee766ad8ba48f968
ChaonengQuan <chaonengquan@email.arizona.edu>
2020-12-05 17:25:03 -0700

    Add: Employee CRUD UI

---
+++ src/main/java/csc460/SqlPlusShoppingMall/Employee.java
@@ -0,0 +1,91 @@
+package csc460.SqlPlusShoppingMall;
+
+public class Employee {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String address;
+    private int phone;
+    private String employeeGroup;
+    private int salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmployeeGroup() {
+        return employeeGroup;
+    }
+
+    public void setEmployeeGroup(String employeeGroup) {
+        this.employeeGroup = employeeGroup;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", address='" + address + '\'' +
+                ", phone=" + phone +
+                ", employeeGroup='" + employeeGroup + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ src/main/java/csc460/SqlPlusShoppingMall/EmployeeController.java
@@ -0,0 +1,33 @@
+package csc460.SqlPlusShoppingMall;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.sql.DataSource;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("")
+    public String employeeFormGet(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "employee";
+    }
+
+    @PostMapping("")
+    public String employeeAdd(@ModelAttribute Employee employee) {
+        String sql = "INSERT INTO chaonengquan.Employee (FirstName, LastName, Gender, Address, Phone, EmployeeGroup, Salary) VALUES (?, ?, ? ,? ,? ,? ,?)";
+        jdbcTemplate.update(sql, employee.getFirstName(), employee.getLastName(), employee.getGender(), employee.getAddress(), employee.getPhone(), employee.getEmployeeGroup(), employee.getSalary());
+        return "resultEmployee";
+    }
+
+
+}
