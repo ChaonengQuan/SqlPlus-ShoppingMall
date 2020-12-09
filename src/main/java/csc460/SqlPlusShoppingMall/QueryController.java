e59b183ac6bf9ac199a12c134a3a76fa04227346
ChaonengQuan <chaonengquan@email.arizona.edu>
2020-12-08 21:39:13 -0700

    add: query1

--- src/main/java/csc460/SqlPlusShoppingMall/QueryController.java
+++ src/main/java/csc460/SqlPlusShoppingMall/QueryController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
@@ -26,45 +24,41 @@ public class QueryController {
 
 
     /**
-     *Write a query that displays the member by searching the phone number or the ID. The results should
+     * Write a query that displays the member by searching the phone number or the ID. The results should
      * display the member name, date of birth, and reward points
      */
     @PostMapping("/1")
-    public String query1(Model model){
+    public String query1(@RequestParam(value = "id", required = false) long id,
+                         @RequestParam(value = "phone", required = false) long phone,
+                         Model model) {
+        
+        String sql = String.format("SELECT * FROM chaonengquan.Supplier WHERE id = %d OR Phone = %d", id, phone);
 
-
-        List<Member> memberList = this.jdbcTemplate.query(
-                "SELECT * FROM chaonengquan.Supplier WHERE id = ? OR Phone = ?",
+        List<Member> memberList = this.jdbcTemplate.query(sql,
                 (rs, rowNum) -> {
                     Member member = new Member();
-                    member.setId(rs.getLong("id"));
                     member.setFirstName(rs.getString("FirstName"));
                     member.setLastName(rs.getString("LastName"));
                     member.setDateOfBirth(rs.getDate("DateOfBirth"));
-                    member.setAddress(rs.getString("Address"));
-                    member.setPhone(rs.getLong("Phone"));
                     member.setRewardPoint(rs.getLong("RewardPoint"));
-                    member.setMembershipPaid(rs.getString("MembershipPaid"));
                     return member;
                 });
 
         model.addAttribute("memberList", memberList);
-        return "hello";
+        return "query1";
     }
 
     @PostMapping("/3")
-    public String query3(Model model){
+    public String query3(Model model) {
         System.out.println("Clicked Query#3");
         return "hello";
     }
 
     @PostMapping("/4")
-    public String query4(Model model){
+    public String query4(Model model) {
         System.out.println("Clicked Query#4");
         return "hello";
     }
-
-
 
 
 }
