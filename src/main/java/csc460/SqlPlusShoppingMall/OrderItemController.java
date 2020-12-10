6bd3223cd2555a45effe655655f9d0fce6b825b3
ChaonengQuan <chaonengquan@email.arizona.edu>
2020-12-09 18:41:38 -0700

    update: OrderItem add/update will update other table that contains related information

--- src/main/java/csc460/SqlPlusShoppingMall/OrderItemController.java
+++ src/main/java/csc460/SqlPlusShoppingMall/OrderItemController.java
@@ -36,7 +36,13 @@ public class OrderItemController {
         String sql = "INSERT INTO chaonengquan.OrderItem (id, SalesRecordID, ProductID, PaidPrice, Quantity) VALUES (?, ?, ?, ?, ?)";
         jdbcTemplate.update(sql, toAdd.getId(), toAdd.getSalesRecordId(), toAdd.getProductId(), toAdd.getPaidPrice(), toAdd.getQuantity());
 
-        String updateSql = String.format("UPDATE chaonengquan.Product SET StockInfo = (SELECT Product.StockInfo - OrderItem.Quantity FROM chaonengquan.Product, chaonengquan.OrderItem WHERE Product.Id = OrderItem.ProductId AND OrderItem.Id = %d)", toAdd.getId());
+        //update SalesRecord totalAmount
+        String updateSalesRecordSql = "UPDATE chaonengquan.SalesRecord SET TotalAmount = (SELECT SUM(OrderItem.PaidPrice * OrderItem.Quantity) FROM chaonengquan.OrderItem, chaonengquan.SalesRecord WHERE OrderItem.SalesRecordId = SalesRecord.Id)";
+        jdbcTemplate.execute(updateSalesRecordSql);
+
+        //update Product stockInfo
+        String updateSql = String.format("UPDATE chaonengquan.Product SET StockInfo = (SELECT Product.StockInfo - OrderItem.Quantity FROM chaonengquan.Product, chaonengquan.Orderitem WHERE Product.Id = OrderItem.ProductId AND OrderItem.Id = %d) WHERE Product.Id IN (SELECT OrderItem.ProductId FROM chaonengquan.OrderItem WHERE OrderItem.Id = %d)",
+                toAdd.getId(), toAdd.getId());
         jdbcTemplate.execute(updateSql);
 
         return "redirect:all";
@@ -61,6 +67,16 @@ public class OrderItemController {
     public String updateOrderItem(@ModelAttribute OrderItem toUpdate) {
         String sql = "UPDATE chaonengquan.OrderItem SET SalesRecordId = ?, ProductId = ?, PaidPrice = ?, Quantity = ? WHERE id = ?";
         jdbcTemplate.update(sql, toUpdate.getSalesRecordId(), toUpdate.getProductId(), toUpdate.getPaidPrice(), toUpdate.getQuantity(), toUpdate.getId());
+
+        //update SalesRecord totalAmount
+        String updateSalesRecordSql = "UPDATE chaonengquan.SalesRecord SET TotalAmount = (SELECT SUM(OrderItem.PaidPrice * OrderItem.Quantity) FROM chaonengquan.OrderItem, chaonengquan.SalesRecord WHERE OrderItem.SalesRecordId = SalesRecord.Id)";
+        jdbcTemplate.execute(updateSalesRecordSql);
+
+        //update Product stockInfo
+        String updateSql = String.format("UPDATE chaonengquan.Product SET StockInfo = (SELECT Product.StockInfo - OrderItem.Quantity FROM chaonengquan.Product, chaonengquan.Orderitem WHERE Product.Id = OrderItem.ProductId AND OrderItem.Id = %d) WHERE Product.Id IN (SELECT OrderItem.ProductId FROM chaonengquan.OrderItem WHERE OrderItem.Id = %d)",
+                toUpdate.getId(), toUpdate.getId());
+        jdbcTemplate.execute(updateSql);
+
         return "redirect:all";
     }
 
